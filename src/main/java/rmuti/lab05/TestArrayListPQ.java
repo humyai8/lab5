26771654e59751b69de08e1284fffba96b2894d2
humyai8 <supachaimos40@gmail.com>
2018-02-07 13:36:55 +0700

    mos

---
+++ src/main/java/rmuti/lab05/TestArrayListPQ.java
@@ -0,0 +1,27 @@
+package rmuti.lab05;
+
+public class TestArrayListPQ {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee("001", "name1", 20_000);
+		Employee emp2 = new Employee("002", "name2", 30_000);
+		Employee emp3 = new Employee("003", "name3", 15_000);
+	
+		ArrayListPQ arrayLisPQ = new ArrayListPQ();
+		arrayLisPQ.enqueue(emp1);
+		arrayLisPQ.enqueue(emp2);
+		arrayLisPQ.enqueue(emp3);
+		
+		System.out.println(arrayLisPQ);
+		
+		arrayLisPQ.dequeue();
+		System.out.println(arrayLisPQ);
+		
+		arrayLisPQ.dequeue();
+		System.out.println(arrayLisPQ);
+		
+		arrayLisPQ.enqueue(new Employee("004", "name4", 40_000));
+		System.out.println(arrayLisPQ);
+	}
+	
+}
